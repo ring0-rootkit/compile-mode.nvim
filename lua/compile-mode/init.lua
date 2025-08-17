c6e36afe2bdbecf0f4d2a12955f82d0170d48f46
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-08-17 12:27:30 +0300

    more highlight

--- lua/compile-mode/init.lua
+++ lua/compile-mode/init.lua
@@ -7,138 +7,149 @@ local vertical_split = true
 local save_args = true
 
 local function create_buffer()
-    local buf = vim.api.nvim_create_buf(true, true)
-    vim.api.nvim_buf_set_name(buf, "*compilation*")
-    return buf
+	local buf = vim.api.nvim_create_buf(true, true)
+	vim.api.nvim_buf_set_name(buf, "*compilation*")
+	return buf
 end
 
 local buf = nil
 
 local function is_buffer_open(buffer_id)
-    local windows = vim.api.nvim_tabpage_list_wins(0)
+	local windows = vim.api.nvim_tabpage_list_wins(0)
 
-    for _, win_id in ipairs(windows) do
-        if vim.api.nvim_win_get_buf(win_id) == buffer_id then
-            return win_id
-        end
-    end
+	for _, win_id in ipairs(windows) do
+		if vim.api.nvim_win_get_buf(win_id) == buffer_id then
+			return win_id
+		end
+	end
 
-    return nil
+	return nil
 end
 
 local function savetolv()
-    if lv then
-        lv["compile_args"] = last_args
-    end
+	if lv then
+		lv["compile_args"] = last_args
+	end
 end
 
 M.compile = function()
-    if last_args == "" then
-        -- prompt user if no argument has been saved yet.
-        print("compile-mode: compile command not set.")
-        return
-    end
+	if last_args == "" then
+		-- prompt user if no argument has been saved yet.
+		print("compile-mode: compile command not set.")
+		return
+	end
 
-    local win = is_buffer_open(buf)
-    if win == nil then
-        if vertical_split then
-            vim.cmd("botright vnew")
-        else
-            vim.cmd("botright new")
-        end
-        win = vim.api.nvim_get_current_win()
-    end
+	local win = is_buffer_open(buf)
+	if win == nil then
+		if vertical_split then
+			vim.cmd("botright vnew")
+		else
+			vim.cmd("botright new")
+		end
+		win = vim.api.nvim_get_current_win()
+	end
 
-    if buf == nil then
-        buf = create_buffer()
-        -- TODO: remove command, maybe add command to go to file
-        -- vim.api.nvim_buf_set_keymap(buf, "n", "q", ":quit<CR>", { noremap = true, silent = true })
-    end
+	if buf == nil then
+		buf = create_buffer()
+		-- TODO: remove command, maybe add command to go to file
+		-- vim.api.nvim_buf_set_keymap(buf, "n", "q", ":quit<CR>", { noremap = true, silent = true })
+	end
 
-    local start_date = vim.fn.strftime("%c")
-    local append_data = function(_, data, event)
-        local end_date = vim.fn.strftime("%c")
-        if event == "stdout" then
-            if data then
-                vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
-            end
-        end
-        if event == "stderr" then
-            if data then
-                vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
-            end
-        end
-        if event == "exit" then
-            vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Compilation finished at " .. end_date })
-        end
-    end
+	local start_date = vim.fn.strftime("%c")
+	local append_data = function(_, data, event)
+		local end_date = vim.fn.strftime("%c")
+		if event == "stdout" then
+			if data then
+				vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+			end
+		end
+		if event == "stderr" then
+			if data then
+				vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+			end
+		end
+		if event == "exit" then
+			vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Compilation finished at " .. end_date })
+		end
+	end
 
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "-*- compile-mode; directory: '" .. vim.fn.getcwd() .. "' -*-" })
-    vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Compilation started at " .. start_date })
-    vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "command: " .. last_args })
-    vim.fn.jobstart(last_args, {
-        stdout_buffered = true,
-        stderr_buffered = true,
-        on_stdout = append_data,
-        on_stderr = append_data,
-        on_exit = append_data,
-    })
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "-*- compile-mode; directory: '" .. vim.fn.getcwd() .. "' -*-" })
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "Compilation started at " .. start_date })
+	vim.api.nvim_buf_set_lines(buf, -1, -1, false, { "command: " .. last_args })
+	vim.fn.jobstart(last_args, {
+		stdout_buffered = true,
+		stderr_buffered = true,
+		on_stdout = append_data,
+		on_stderr = append_data,
+		on_exit = append_data,
+	})
 
-    vim.api.nvim_win_set_buf(win, buf)
+	vim.api.nvim_win_set_buf(win, buf)
 end
 
 M.compile_setup = function(opts)
-    -- sets the arguments to be executed.
-    if next(opts.fargs) == nil then
-        last_args = vim.fn.input({
-            prompt = "Compile command: ",
-            default = last_args,
-        })
+	-- sets the arguments to be executed.
+	if next(opts.fargs) == nil then
+		last_args = vim.fn.input({
+			prompt = "Compile command: ",
+			default = last_args,
+		})
 
-        if last_args == "" then
-            return
-        end
-    else
-        last_args = opts.args
-    end
+		if last_args == "" then
+			return
+		end
+	else
+		last_args = opts.args
+	end
 
-    if save_args then
-        savetolv()
-    end
-    M.compile()
+	if save_args then
+		savetolv()
+	end
+	M.compile()
 end
 
 M.setup = function(opts)
-    if opts then
-        if opts.save_args then
-            save_args = opts.save_args
-        end
-        if opts.vertical_split then
-            vertical_split = opts.vertical_split
-        end
-    end
+	if opts then
+		if opts.save_args then
+			save_args = opts.save_args
+		end
+		if opts.vertical_split then
+			vertical_split = opts.vertical_split
+		end
+	end
 
-    vim.api.nvim_create_user_command("Compile", M.compile_setup, { nargs = "*" })
-    vim.api.nvim_create_user_command("Recompile", M.compile, {})
-    vim.api.nvim_create_user_command("CompileSplitToggle", function()
-        vertical_split = not vertical_split
-    end, {})
+	vim.api.nvim_create_user_command("Compile", M.compile_setup, { nargs = "*" })
+	vim.api.nvim_create_user_command("Recompile", M.compile, {})
+	vim.api.nvim_create_user_command("CompileSplitToggle", function()
+		vertical_split = not vertical_split
+	end, {})
 
-    vim.api.nvim_create_autocmd('BufWinEnter', {
-      pattern = '*compilation*',
-      callback = function()
-        vim.cmd([[
-          syntax match MyHeader /-\*- compile-mode;.* -\*-/
-          syntax match MyParam /Compilation started at .*/
-          syntax match MyParam /Compilation finished at .*/
-          syntax match MyParam /command:/
+	vim.api.nvim_create_autocmd('BufWinEnter', {
+		pattern = '*compilation*',
+		callback = function()
+			vim.cmd([[
+				syntax match CompHeader /-\*- compile-mode;.* -\*-/
+				syntax match CompParam /Compilation started at .*/
+				syntax match CompParam /Compilation finished at .*/
+				syntax match CompParam /command:/
 
-          highlight link MyHeader Title
-          highlight link MyParam Identifier
-        ]])
-      end
-    })
+				syntax match CompError /^error/
+				syntax match CompError / error/
+				syntax match CompError /^err/
+				syntax match CompError / err/
 
+				syntax match CompWarn /^warning/
+				syntax match CompWarn / warning/
+				syntax match CompWarn /^warn/
+				syntax match CompWarn / warn/
+
+				highlight link CompHeader Title
+				highlight link CompParam Identifier
+				highlight link CompError Error
+				highlight link CompWarn WarningMsg
+			]])
+		end
+	})
 end
 
 return M
